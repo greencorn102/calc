389399b231d9d45738edb6e6223bab4a238f552f
greencorn102 <126756036+greencorn102@users.noreply.github.com>
2025-12-28 15:34:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ import numpy as np
 app = FastAPI()
 
 # Load once when server starts
-model = joblib.load("svm_model.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model = joblib.load(os.path.join(BASE_DIR, "svm_model.pkl"))
 
 @app.post("/predict")
 def predict(features: list):
     x = np.array(features).reshape(1, -1)
     prediction = model.predict(x)
     return {"prediction": int(prediction[0])}
+
