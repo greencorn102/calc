813ad732590121a6256200df37a43be08ed5dd38
greencorn102 <126756036+greencorn102@users.noreply.github.com>
2025-12-28 15:58:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
 import numpy as np
 import os
 
 app = FastAPI()
 
-# Load once when server starts
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 model = joblib.load(os.path.join(BASE_DIR, "svm_model.pkl"))
 
+class PredictInput(BaseModel):
+    features: list[float]
+
 @app.post("/predict")
-def predict(features: list):
-    x = np.array(features).reshape(1, -1)
+def predict(data: PredictInput):
+    x = np.array(data.features).reshape(1, -1)
     prediction = model.predict(x)
     return {"prediction": int(prediction[0])}
-
-
