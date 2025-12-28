4a1c2fb90b3368ef959c161ac69097390b41acfa
greencorn102 <126756036+greencorn102@users.noreply.github.com>
2025-12-27 23:36:25 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# Define input format
+class AddInput(BaseModel):
+    a: float
+    b: float
+
+# Define API endpoint
+@app.post("/add")
+def add_numbers(data: AddInput):
+    result = data.a - data.b
+    return {
+        "a": data.a,
+        "b": data.b,
+        "sum": result
+    }
